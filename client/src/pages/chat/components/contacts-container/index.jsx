750596cd97882d096a5ee9995401d37618408111
madhuramachiraju-ui <madhura.machiraju2023@vitstudent.ac.in>
2025-03-05 12:08:42 +0530

    Update index.jsx

--- client/src/pages/chat/components/contacts-container/index.jsx
+++ client/src/pages/chat/components/contacts-container/index.jsx
@@ -7,3 +7,31 @@ const ContactsContainer= () => {
 };
 
 export default ContactsContainer;
+
+const Logo = () => {
+    return (
+        <div className = "flex p-5 justify-start items-center gap-2">
+        <svg
+            id="logo-38"
+            width="78"
+            height="32"
+            viewBox="0 0 78 32"
+            fill= "none"
+            xmins= "http://www.w3.org/2000/svg"
+            >
+            {" "}
+        <path
+            d= "M35.5 Oh31.5L12.5 32H16.5L35.5 02"
+            className = "ccustom"
+            fill="<#8338ec"
+            ></path>{" "}
+            <path
+                d="M19.5 0H31.5L12.5 32H0.5L19.5 02"
+                className="ccompli2"
+                fill = "#a16ee8"
+                ></path>{" "}
+        </svg>
+            <span className= "text-3xl font-semibold ">Syncronus</span>/div>
+            );
+            };
+            
